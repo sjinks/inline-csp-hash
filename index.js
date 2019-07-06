e7fc75a294955f8406ea4296971e08ae54d52fa9
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2019-07-06 15:44:24 +0300

    semistandard + bug fixes

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-const cheerio  = require('cheerio');
-const crypto   = require('crypto');
+const cheerio = require('cheerio');
+const crypto = require('crypto');
 const through2 = require('through2');
 
 const defaults = {
-	what:       'script',
-	hash:       'sha256',
-	replace_cb: null
+  what: 'script',
+  hash: 'sha256',
+  replace_cb: null
 };
 
-function mapItems(html, what, callback)
-{
-	const $ = cheerio.load(html);
-	return $(what + ':not([src])').map((i, el) => callback($(el).text())).toArray();
+function mapItems (html, what, callback) {
+  const $ = cheerio.load(html);
+  return $(what + ':not([src])').map((i, el) => callback($(el).text())).toArray();
 }
 
-function hashstream(opts)
-{
-	opts = Object.assign({}, defaults, opts);
+function hashstream (opts) {
+  opts = Object.assign({}, defaults, opts);
 
-	if (!/^sha(256|384|512)$/.test(opts.hash)) {
-		throw new Error("Only sha256/384/512 hashes are supported.");
-	}
+  if (!/^sha(256|384|512)$/.test(opts.hash)) {
+    throw new Error('Only sha256/384/512 hashes are supported.');
+  }
 
-	const hash = function(s) {
-		return crypto.createHash(opts.hash).update(s).digest('base64');
-	};
+  const hash = function (s) {
+    return crypto.createHash(opts.hash).update(s).digest('base64');
+  };
 
-	const hashes = [];
-	return through2.obj((file, enc, callback) => {
-		const content = file.contents;
-		const result  = mapItems(content, opts.what, hash).map(h => `'${opts.hash}-${h}'`);
-		hashes.push(...result);
+  const hashes = [];
+  return through2.obj((file, enc, callback) => {
+    const content = file.contents;
+    const result = mapItems(content, opts.what, hash).map(h => `'${opts.hash}-${h}'`);
+    hashes.push(...result);
 
-		if (typeof opts.replace_cb === 'function') {
-			const s = opts.replace_cb(content.toString(), hashes);
-			file.contents = new Buffer(s, enc);
-		}
+    if (typeof opts.replace_cb === 'function') {
+      const s = opts.replace_cb(content.toString(), hashes);
+      file.contents = Buffer.from(s, enc);
+    }
 
-		callback(null, file);
-	});
+    callback(null, file);
+  });
 }
 
 module.exports = hashstream;
